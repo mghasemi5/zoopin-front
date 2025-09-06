1620211c120d4651b2a6aedf8b68b6c38e804b75
mghasemi5 <mohammadghasemi5@gmail.com>
2025-09-06 17:34:03 +0330

    v1.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 // src/config.js (CRA)
-//export const API_BASE =
-//  (process.env.REACT_APP_API_BASE || (window.location.origin ));
-
-export const API_BASE = "http://127.0.0.1:8000"
+export const API_BASE =
+  (process.env.REACT_APP_API_BASE || (window.location.origin ));
 
